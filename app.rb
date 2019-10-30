1588736a49ea0b894678ecd16bad9eed91ba17f7
Jenny Chu <jchudesign0930@gmail.com>
2019-10-30 23:11:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,35 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @reversename = params[:name]
+    "#{@reversename.reverse}"
+  end
+  
+  get '/square/:number' do
+    @num = params[:number].to_i**2
+    
+    "#{params[:number].to_i**2}"
+  end
+  
+  
+  get '/say/:number/:phrase' do
+    str = ""
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @num.times { str += "#{@phrase}\n" }
+    str
+    
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]*params[:word2]*params[:word3]*params[:word4]*params[:word5]}"
+  
+  end
+  
+  get '/:operation/:number1/:number2' do
+  
+  end
+  
 
 end
